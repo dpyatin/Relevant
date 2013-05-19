e9842567b1f6e7667d885b7631b3522417bc80bb
Dmitry <dmitry.pyatin@gmail.com>
2013-05-18 23:52:40 -0400

    App deployed, json rendered

--- Chrome extension/main.js
+++ Chrome extension/main.js
@@ -12,21 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
   		else {
   			var id = document.getElementById('twitterid').value;
   			var req = new XMLHttpRequest();
-  			req.open("GET", "http://angelhack-travel-253.usw1.actionbox.io:5000/api/getBookRecommendation/?twitterHandle=" + id, true);
+  			req.open("GET", "http://stormy-dusk-3543.herokuapp.com/api/getBookRecommendation/?twitterHandle=" + id, true);
   			req.onreadystatechange = function() {
     			if (req.readyState == 4) {
     				var bookHtml = "<div>";
         			var book = JSON.parse(req.responseText);
-        			document.getElementById('dataParagraph').innerHTML = "SUCCESS...OMFG\n" + book;
-        			for (var key in book) {
-        				bookHtml += "<p>" + book[key] + "</p>";
-        			}
+        			//document.getElementById('dataParagraph').innerHTML = "SUCCESS...OMFG\n" + book.title;
+        			bookHtml += "<div style=\"float:left;\"><img src=" + book.image + " alt=\"Smiley face\" height=\"80\" width=\"42\"></div>";
+        			bookHtml += "<div><p style=\"font:garamont\"><i>" + book.quote + "</i></p></div>";
+        			bookHtml += "<a target=\"_blank\" href=" + book.link + ">" + book.title + "</a>";
         			bookHtml += "</div>";	
-        			var parent = document.getElementById('dataParagraph');
-        			parent.insertAfter(bookHtml, parent.firstChild);
+        			$("#inputContainer").empty();
+        			$("#bookRecommendations").html(bookHtml);
     			}
 			}
-    		//req.onload = handleBookMetadata.bind(this);
     		req.send(null);
   		}
 	}
