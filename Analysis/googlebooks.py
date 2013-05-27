04fee0be3fd79801f6339bf82e583c584735fcd1
Lucas Lemanowicz <lemanowiczlucas@gmail.com>
2013-05-27 22:28:16 +0000

    Fixed and refactored GoogleBooks implementation

--- Analysis/googlebooks.py
+++ Analysis/googlebooks.py
@@ -1,63 +1,65 @@
-#!/usr/bin/python
-#
-# Copyright 2012 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import pprint
-import sys
 from apiclient.discovery import build
-
-# For this example, the API key is provided as a command-line argument.
-
-def getExcerpt(quote, image, title, author, tags):
-
+# from googlebooks import GoogleBooks; gb = GoogleBooks(); gb.getExcerpt({'quote': 'politics', 'title': 'kennedy'})
+class GoogleBooks:
 	api_key = "AIzaSyAnwBRAP94niYqybkY8QkAIvG7QqbDiicQ"
 
-	# The apiclient.discovery.build() function returns an instance of an API service
-	# object that can be used to make API calls. The object is constructed with
-	# methods specific to the books API. The arguments provided are:
-	#   name of the API ('books')
-	#   version of the API you are using ('v1')
-	#   API key
-	service = build('books', 'v1', developerKey=api_key)
+	def getExcerpt(self, book_info):
+		book_result = {}
 
-	# The books API has a volumes().list() method that is used to list books
-	# given search criteria. Arguments provided are:
-	#   volumes source ('public')
-	#   search query ('android')
-	# The method returns an apiclient.http.HttpRequest object that encapsulates
-	# all information needed to make the request, but it does not call the API.
+		response = self._search_book(book_info)
 
-	passBook = quote + "+inline=" + title
+		for book in response.get('items', []):
+			book_result = self._parse_book(book)
 
-	request = service.volumes().list(source='public', q=passBook)
+			if book_result is not None:
+				return book_result
 
-	# The execute() function on the HttpRequest object actually calls the API.
-	# It returns a Python object built from the JSON response. You can print this
-	# object or refer to the Books API documentation to determine its structure.
-	response = request.execute()
-	#pprint.pprint(response)
+		return book_result
 
-	# Accessing the response like a dict object with an 'items' key returns a list
-	# of item objects (books). The item object is a dict object with a 'volumeInfo'
-	# key. The volumeInfo object is a dict with keys 'title' and 'authors'.
-	#print 'Found %d books:' % len(response['items'])
-	
-	for book in response.get('items', []):
-		if(book['volumeInfo']['textSnippet'] is not None):
-	  		return((book['volumeInfo']['title'],
-		    book['volumeInfo']['authors'], 
-		    book['searchInfo']['textSnippet']))
-		
 
+	def _search_book(self, book_info):
+		""" Queries Google Books based on Book Info
+		In particular it looks at the quote and book title
+		"""
+		response = []
+
+		try:
+			service = build('books', 'v1', developerKey=self.api_key)
+
+			query = {
+				'source': 'public',
+				'q': book_info['quote'] + "+intitle:" + book_info['title'] }
+
+			request = service.volumes().list(**query)
+			request.uri += "&country=US" # Hack
+			response = request.execute()
+		except:
+			pass
+
+		return response
+
+
+	def _parse_book(self, book):
+		""" Parses Title, Author and Excerpt from Google Book API Call
+		Returns dict with {title, author, excerpt}
+		"""
+
+		result = None
+		try:
+			title = book['volumeInfo']['title']
+			title = title.encode('ascii', 'ignore')
+
+			author = book['volumeInfo']['authors'][0]
+			author = author.encode('ascii', 'ignore')
+
+			excerpt = book['searchInfo']['textSnippet']
+			excerpt = excerpt.encode('ascii', 'ignore')
+			
+			result = {
+				'title': title,
+				'author': author,
+				'excerpt': excerpt }
+		except:
+			pass
+
+		return result
